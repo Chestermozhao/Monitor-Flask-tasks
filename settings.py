b9f89613c0c02fc58ef0eca4dd0ed70f9afa8428
Chester Mo <chester_mo@trendmicro.com>
2021-03-31 21:04:49 +0800

    Update: using pre-commit hook with black code style and add decouple to read env file

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
+from decouple import config
 
-CELERY_BROKER_URL='redis://localhost:6379/0'
-CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 # flask mail config
 # set low security for your sender mail account
@@ -10,6 +10,6 @@ CELERY_RESULT_BACKEND='redis://localhost:6379/0'
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 465
 MAIL_USE_SSL = True
-MAIL_USERNAME = "<your-username>"
-MAIL_PASSWORD = os.getenv('email_pwd')
-MAIL_DEFAULT_SENDER = "<your-email-account>"
+MAIL_USERNAME = config("MAIL_USERNAME")
+MAIL_PASSWORD = config("EMAIL_PWD")
+MAIL_DEFAULT_SENDER = config("MAIL_SENDER")
